a6bbc332910ad4818e3803b04b7cbcbeec72a08c
Luan Freitas <luandkg@gmail.com>
2024-08-22 11:34:41 -0300

    Parte 2 concluida finalmente

--- day_12.py
+++ day_12.py
@@ -32,14 +32,18 @@ F11"""
     print(" + PROBLEMA EXTRA DA INTRODUÇÃO")
     print("")
 
-    entrada_texto =""""""
+    entrada_texto ="""F10
+N3
+F7
+R90
+F11"""
 
-    #solucao_parte_extra(transformar_texto_em_entradas(entrada_texto))
+    solucao_parte_extra(transformar_texto_em_entradas(entrada_texto))
 
     print("")
     print(" + PROBLEMA EXTRA")
     print("")
-    #solucao_parte_extra(transformar_texto_em_entradas(obter_entradas("inputs/input_" + ADVENT_OF_CODE + ".txt")))
+    solucao_parte_extra(transformar_texto_em_entradas(obter_entradas("inputs/input_" + ADVENT_OF_CODE + ".txt")))
 
 
 def transformar_texto_em_entradas(entrada_texto):
@@ -61,8 +65,6 @@ def obter_entradas(arquivo_caminho):
 
 
 def solucao(entradas):
-
-    EAST = 1
 
     direcoes = ["N","S","E","W","L","R","F"]
 
@@ -128,7 +130,112 @@ def solucao(entradas):
 
 
 def solucao_parte_extra(entradas):
-    pass
+
+    direcoes = ["N","S","E","W","L","R","F"]
+
+    NORTH_SOUTH = 0
+    EAST_WEST   = 0
+    DIRECTION = direcoes[2]
+
+    print("Iniciando percurso em Manhattan : ")
+    print()
+    print("\t -->> " + DIRECTION)
+    print()
+
+    direcionadores = {"N":"NORTH","S":"SOUTH","E":"EAST","W":"WEST"}
+    unidades = {"NORTH":0,"SOUTH":0,"EAST":0,"WEST":0}
+    waypoints = {"NORTH":1,"SOUTH":0,"EAST":10,"WEST":0}
+
+    DIRECOES = ["N","E","S","W"]
+
+    executando = 0
+
+    for entrada in entradas:
+
+        ir_para = entrada[0]
+        ir_quantidade = int(entrada[1:])
+        print("\t ++ Entrada : " + espacar(entrada,10) + " :: " + ir_para + " :: " + str(ir_quantidade))
+
+        if ir_para == "F":
+            #valores[direcionadores[DIRECTION]] +=ir_quantidade
+
+            for direcao_alterar in DIRECOES:
+                unidades[direcionadores[direcao_alterar]] +=(ir_quantidade*waypoints[direcionadores[direcao_alterar]]) 
+
+
+        elif ir_para == "N" or ir_para == "E" or ir_para == "S" or ir_para == "W":
+            waypoints[direcionadores[ir_para]] +=ir_quantidade
+        elif ir_para == "R":
+            print("Estou em :: " + DIRECTION)
+            de = obter_direcao(DIRECTION,DIRECOES)
+            alterar = int(ir_quantidade / 90)
+            nova_direcao = (de + alterar) % 4
+           
+            DIRECTION = DIRECOES[nova_direcao]
+            print("Mudar :: " + str(de) + " com " + str(alterar) + " = " + str(nova_direcao) + " -->> " + DIRECTION)
+
+            waypoints_copia = {"NORTH":0,"SOUTH":0,"EAST":0,"WEST":0}
+
+            for direcao_corrente in DIRECOES:
+                waypoints_copia[direcionadores[direcao_corrente]] =  waypoints[direcionadores[direcao_corrente]]
+
+            for direcao_corrente in DIRECOES:
+                valor_direcao = waypoints_copia[direcionadores[direcao_corrente]]
+                de = obter_direcao(direcao_corrente,DIRECOES)
+                nova_direcao = (de + alterar) % 4
+                waypoints[direcionadores[DIRECOES[nova_direcao]]] = valor_direcao
+
+            
+
+        elif ir_para == "L":
+            print("Estou em :: " + DIRECTION)
+            de = obter_direcao(DIRECTION,DIRECOES)
+            alterar = int(ir_quantidade / 90)
+            nova_direcao = (de - alterar)  
+            while nova_direcao<0:
+                nova_direcao+=4
+           
+            DIRECTION = DIRECOES[nova_direcao]
+            print("Mudar :: " + str(de) + " com " + str(alterar) + " = " + str(nova_direcao) + " -->> " + DIRECTION)
+
+            waypoints_copia = {"NORTH":0,"SOUTH":0,"EAST":0,"WEST":0}
+
+            for direcao_corrente in DIRECOES:
+                waypoints_copia[direcionadores[direcao_corrente]] =  waypoints[direcionadores[direcao_corrente]]
+
+            for direcao_corrente in DIRECOES:
+                valor_direcao = waypoints_copia[direcionadores[direcao_corrente]]
+                de = obter_direcao(direcao_corrente,DIRECOES)
+                nova_direcao = (de - alterar) % 4
+                while nova_direcao<0:
+                    nova_direcao+=4
+
+                waypoints[direcionadores[DIRECOES[nova_direcao]]] = valor_direcao
+
+
+
+        NORTH_SOUTH = abs(int(unidades["NORTH"]) - int(unidades["SOUTH"]))
+        EAST_WEST = abs(int(unidades["EAST"]) - int(unidades["WEST"]))
+
+        print("\t\t :: DIR         = " + DIRECTION)
+
+        print("Unidades")
+        for direcao_alterar in DIRECOES:
+                valor_local = unidades[direcionadores[direcao_alterar]] 
+                print("\t\t :: " + direcao_alterar + " :: " + str(valor_local))
+
+        print("WayPoints")
+        for direcao_alterar in DIRECOES:
+                valor_local = waypoints[direcionadores[direcao_alterar]] 
+                print("\t\t :: " + direcao_alterar + " :: " + str(valor_local))
+
+        print()
+        print("\t\t :: NORTH_SOUTH = " + str(NORTH_SOUTH))
+        print("\t\t :: EAST_WEST   = " + str(EAST_WEST))
+        print("\t\t :: VALOR       = " + str(NORTH_SOUTH+EAST_WEST))
+
+        executando+=1
+  
 
 
 def espacar(s,tamanho):
